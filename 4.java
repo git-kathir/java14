5107962a44421b9c459310c7db1b68dc4e777d19
git-kathir <kathirkathir8056@gmail.com>
2025-02-14 15:54:35 +0530

    Create 4.java

---
+++ 4.java
@@ -0,0 +1,20 @@
+class Solution {
+
+public boolean contains Duplicate(int[] nums) {
+
+Set<Integer> set = new HashSet();
+
+for(int 1: nums) [
+
+set.add(1);
+
+}
+if(nums.length== set.size()){
+
+return false;
+
+}
+
+return true;
+}
+}
